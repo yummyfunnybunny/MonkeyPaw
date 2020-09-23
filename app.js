fa621702482e23650dac58982bc30646b0f3f611
gonzo-code <toormund@protonmail.com>
2020-09-23 19:08:29 -0400

    testing PR

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ farmButton.addEventListener('click', success);
 // ==========================
 // FUNCTIONS
 // ==========================
+
+// Testing out something
 function success() {
   // set confetti parameters
   confetti.maxCount = 150;     //set max confetti count
@@ -18,7 +20,7 @@ function success() {
 	confetti.frameInterval = 20; //the confetti animation frame interval in milliseconds
 	confetti.alpha = 1;        //the alpha opacity of the confetti (between 0 and 1, where 1 is opaque and 0 is invisible)
   confetti.gradient = false;   //whether to use gradients for the confetti particles
-  
+
   confetti.start(2000);
 
   var audio = new Audio('birthdaySound.mp3');
