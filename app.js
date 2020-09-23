96f3217b628b79a91e0ef3afac5fb6d922c5e734
unknown <yummyfunnybunny13@gmail.com>
2020-09-23 19:13:12 -0400

    small changes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ farmButton.addEventListener('click', success);
 function success() {
   // set confetti parameters
   confetti.maxCount = 150;     //set max confetti count
-	confetti.speed = 2;          //set the particle animation speed
+	confetti.speed = 10;          //set the particle animation speed
 	confetti.frameInterval = 20; //the confetti animation frame interval in milliseconds
 	confetti.alpha = 1;        //the alpha opacity of the confetti (between 0 and 1, where 1 is opaque and 0 is invisible)
   confetti.gradient = false;   //whether to use gradients for the confetti particles
